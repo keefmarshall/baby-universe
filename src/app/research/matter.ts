6393ad119edab74e672835ff2bc0ad8029080f56
Keith Marshall <keith.marshall@bcsg.com>
2017-11-10 07:54:54 +0000

    Various matter research projects. Class diagram

--- src/app/research/matter.ts
+++ src/app/research/matter.ts
@@ -1,6 +1,69 @@
 import { ResearchProject } from "app/research/research-project";
 import { Universe } from "app/services/universe";
 import { PhotonCollector } from "app/machines/photon-collector";
+import { PhotonicPhilosopher } from "app/machines/photonic-philosopher";
+
+
+export class Fermions extends ResearchProject {
+
+    constructor() {
+        super("Matter: Fermions", "The fundamental building blocks of Matter", 2);
+    }
+
+    preconditions(universe: Universe): boolean {
+        return this.machineQuantity(universe, PhotonicPhilosopher.name) > 0;
+    }
+
+    onCompletion(universe: Universe) {
+    }
+
+}
+
+export class Quarks1 extends ResearchProject {
+
+    constructor() {
+        super("Matter: Quarks I", "The most common flavours of quark", 3);
+    }
+
+    preconditions(universe: Universe): boolean {
+        return this.isResearched(universe, new Fermions());
+    }
+
+    onCompletion(universe: Universe) {
+    }
+
+}
+
+export class Quarks2 extends ResearchProject {
+
+    constructor() {
+        super("Matter: Quarks II", "Heavier quarks", 10);
+    }
+
+    preconditions(universe: Universe): boolean {
+        return this.isResearched(universe, new Quarks1());
+    }
+
+    onCompletion(universe: Universe) {
+    }
+
+}
+
+export class Quarks3 extends ResearchProject {
+
+    constructor() {
+        super("Matter: Quarks III", "The heaviest quarks", 50);
+    }
+
+    preconditions(universe: Universe): boolean {
+        return this.isResearched(universe, new Quarks2());
+    }
+
+    onCompletion(universe: Universe) {
+    }
+
+}
+
 
 export class Leptons extends ResearchProject {
 
@@ -9,7 +72,8 @@ export class Leptons extends ResearchProject {
     }
 
     preconditions(universe: Universe): boolean {
-        return this.machineQuantity(universe, PhotonCollector.name) >= 50;
+        return this.machineQuantity(universe, PhotonCollector.name) >= 50 &&
+            universe.phase > 1;
     }
 
     onCompletion(universe: Universe) {
--- src/app/research/research-list.ts
+++ src/app/research/research-list.ts
@@ -1,7 +1,8 @@
 import { KineticConstruction, KineticEngineering } from "app/research/kinetics";
 import { ResearchProject } from "app/research/research-project";
-import { Leptons } from "app/research/matter";
-import { Photovoltaics, LinearPolarisation, CircularPolarisation, EllipticalPolarisation, Photoelectrics, Reflection } from "app/research/photons";
+import { Leptons, Fermions, Quarks3, Quarks2, Quarks1 } from "app/research/matter";
+import { Photovoltaics, LinearPolarisation, CircularPolarisation, EllipticalPolarisation,
+     Photoelectrics, Reflection } from "app/research/photons";
 
 export class ResearchList {
     public projectList: Array<ResearchProject> = [];
@@ -17,6 +18,10 @@ export class ResearchList {
             new Reflection(),
             new KineticConstruction(),
             new KineticEngineering(),
+            new Fermions(),
+            new Quarks1(),
+            new Quarks2(),
+            new Quarks3(),
             new Leptons()
         ];
 
