662f58cc7674bfdab1b1ea39a228de5cf826c945
Keith Marshall <code@eleusis.co.uk>
2018-03-02 08:47:48 +0000

    more neatening

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 // This is a module to encapsulate the material design components
