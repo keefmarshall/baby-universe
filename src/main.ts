061a686ad62d66fbe01db789f3287b50ddd80e64
Keith Marshall <code@eleusis.co.uk>
2018-03-02 08:48:31 +0000

    fontawesome5 integration

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,13 @@ import 'hammerjs';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import { faThermometerHalf } from '@fortawesome/fontawesome-free-solid';
+import fontawesome from '@fortawesome/fontawesome';
+
 if (environment.production) {
   enableProdMode();
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule);
+
+fontawesome.library.add(faThermometerHalf);
