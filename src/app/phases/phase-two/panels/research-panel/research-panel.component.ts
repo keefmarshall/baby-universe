370cc077c5d0419042d0adf9fc26720458fd75db
Keith Marshall <code@eleusis.co.uk>
2018-03-25 18:38:24 +0100

    Research panel styling

--- src/app/phases/phase-two/panels/research-panel/research-panel.component.ts
+++ src/app/phases/phase-two/panels/research-panel/research-panel.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UniverseService } from '../../../../services/universe.service';
 
 @Component({
   selector: 'phase-two-research-panel',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResearchPanelComponent implements OnInit {
 
-  constructor() { }
+  constructor(public universeService: UniverseService) { }
 
   ngOnInit() {
   }
